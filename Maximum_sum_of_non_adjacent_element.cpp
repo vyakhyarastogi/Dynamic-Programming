537dfac93e627cf2ea7c94eeae01c06210b93674
Vyakhya Rastogi <111945663+vyakhyarastogi@users.noreply.github.com>
2023-06-24 17:43:04 +0530

    Update Maximum_sum_of_non_adjacent_element.cpp

--- Maximum_sum_of_non_adjacent_element.cpp
+++ Maximum_sum_of_non_adjacent_element.cpp
@@ -21,6 +21,47 @@ int solveUsingMem(int index, vector<int> &arr, vector<int> &dp)
     return dp[index] = max(pick, notPick);
 }
 
+int solveUsingTab(vector<int> &arr, int n)
+{
+    vector<int> dp(n, -1);
+    dp[0] = arr[0];
+
+    for (int i = 1; i < n; i++)
+    {
+        int pick = arr[i];
+        if (i > 1)
+        {
+            pick += dp[i - 2];
+        }
+        int nonPick = 0 + dp[i - 1];
+
+        dp[i] = max(pick, nonPick);
+    }
+
+    return dp[n - 1];
+}
+
+int spaceOptimisation(int n, vector<int> &arr)
+{
+    int prev = arr[0];
+    int prev2 = 0;
+
+    for (int i = 1; i < n; i++)
+    {
+        int pick = arr[i];
+        if (i > 1)
+        {
+            pick += prev2;
+        }
+        int notPick = 0 + prev;
+
+        int curr = max(pick, notPick);
+        prev2 = prev;
+        prev = curr;
+    }
+    return prev;
+}
+
 int solve(int n, vector<int> &arr)
 {
     vector<int> dp(n, -1);
